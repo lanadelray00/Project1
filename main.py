47b06836aff424d4e270a6cab56cfdca2d819c58
mingu4u <jumphbs1234@gmail.com>
2023-10-24 11:28:34 +0900

    DB_code_1.1

--- main.py
+++ main.py
@@ -28,17 +28,17 @@ check in, check out이 1 이 될때만
 
 
 def f1_execute_query_nonvideo(connectionf1_count, f1_inout, f1_timelog, f1_gate_mode, f1_message):
-    global dbon, state
+    global dbon, check_in, check_out
     cursor = connection.cursor()
     while dbon:
-        # if state != 'none':
-        sql_insert = "insert into iot_project (f1_count, f1_inout, f1_timelog, f1_gate_mode, f1_message) value (%s, %s, %s, %s, %s)"
-        cursor.execute(sql_insert, tuple(()))
-        result = cursor.fetchall()
+        if check_in == 1 or check_out == 1:
+            sql_insert = "insert into iot_project (f1_count, f1_inout, f1_timelog, f1_gate_mode, f1_message) value (%s, %s, %s, %s, %s)"
+            cursor.execute(sql_insert, tuple(()))
+            result = cursor.fetchall()
 
-        for row in result:
-            # 결과 처리
-            print(row)
+            for row in result:
+                # 결과 처리
+                print(row)
         # time.sleep(1)  # 1초마다 쿼리 실행
     cursor.close()
     connection.close()
@@ -49,7 +49,15 @@ def f1_execute_query_nonvideo(connectionf1_count, f1_inout, f1_timelog, f1_gate_
 connection = create_mysql_connection()
 
 dbon = True
-state = 'none'
+check_in = 0
+check_out = 0
+
+
+
+# def tempSpace():
+    # check_in or check_out이 1이 들어오면 tempSpace queue에 저장
+    # f1_execute_query_nonvideo 업데이트 시 저장된 데이터들을 모두 전달해줌. 이때, queue는 전부 clear.
+     
 query_thread = threading.Thread(target=f1_execute_query_nonvideo,args=(connection, f1_count, f1_inout, f1_timelog, f1_gate_mode, f1_message))
 
 query_thread.start()
