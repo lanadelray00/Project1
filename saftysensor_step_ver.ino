833038745ac56a7fa21bc941ac8926a10f895ec3
changmi-kim <141194237+changmi-kim@users.noreply.github.com>
2023-10-25 10:18:05 +0900

    아두이노_스텝모터,초음파1_ver2_코드 정리, 프린트문 수정

--- saftysensor_step_ver.ino
+++ saftysensor_step_ver.ino
@@ -19,62 +19,45 @@ void setup() {
   Serial.begin(9600);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
-
   myStepper.setSpeed(300);
 }
+
 void loop() {
   unsigned long t_now = millis();
   if(t_now - t_prev >= t_delay) {
     t_prev = t_now;
-
   
-  if (i == 0) {
-    flag = 1;
-  }
-  
-  if(flag == 1) {   
-      i++;
-  // 시계 반대 방향으로 한바퀴 회전
-  myStepper.step(stepsPerRevolution);
-
-  unsigned long duration, distance;
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-  duration = pulseIn(ECHO, HIGH);
-  distance = duration * 17 / 1000;
-  Serial.print("\nDistance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  Serial.print(i);
-  }
-  if(i == 33) {
-    flag = -1;
-  }
-
-  if (flag == -1) {
-    i--;
+    if (i == 0) {
+      flag = 1;
+    }
+    
+    if(flag == 1) {   
+        i++;
     // 시계 반대 방향으로 한바퀴 회전
-  myStepper.step(-stepsPerRevolution);
+    myStepper.step(stepsPerRevolution);
+    }
 
-  unsigned long duration, distance;
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-  duration = pulseIn(ECHO, HIGH);
-  distance = duration * 17 / 1000;
-  Serial.print("\nDistance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+    if(i == 33) {
+      flag = -1;
+    }
 
-  Serial.print(i); 
+    if (flag == -1) {
+      i--;
+    // 시계 반대 방향으로 한바퀴 회전
+    myStepper.step(-stepsPerRevolution);
+    }
+
+    unsigned long duration, distance;
+    digitalWrite(TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+    duration = pulseIn(ECHO, HIGH);
+    distance = duration * 17 / 1000;
+    Serial.print("\nDistance:");
+    Serial.println(distance);
+    // Serial.println("cm");
+    // Serial.print(i);
   }
-  
-  }
-
 }
