f5af166b1e05f868ec72948bb6cb7706c8cd577e
changmi-kim <141194237+changmi-kim@users.noreply.github.com>
2023-10-27 15:21:11 +0900

    pyserial_스텝모터,초음파센서2_ver1_f2 6가지 값 DB 업데이트
    6가지 값은 f2_id, f2_index, f2_dist_1, f2_dist_2, f2_timelog, f2_stepmotor_angle);

--- pyserial_saftysensor.py
+++ pyserial_saftysensor.py
@@ -5,11 +5,11 @@ from datetime import datetime
 import time
 
 # '----------------아두이노와 통신----------------'
-ardu = serial.Serial(port='/dev/ttyACM0', baudrate=9600,
-                     parity=serial.PARITY_NONE,
-                     stopbits=serial.STOPBITS_ONE,
-                     bytesize=serial.EIGHTBITS)
-# ardu = serial.Serial('/dev/ttyACM0', 9600, timeout=0.1)
+# ardu = serial.Serial(port='/dev/ttyACM0', baudrate=9600,
+#                      parity=serial.PARITY_NONE,
+#                      stopbits=serial.STOPBITS_ONE,
+#                      bytesize=serial.EIGHTBITS)
+ardu = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 time.sleep(2)  # 접속 대기
 
 # '-------------------DB 연결-------------------'
@@ -25,18 +25,19 @@ def Connect():
 # '---------------데이터테이블 생성----------------'
 # remote = Connect()
 # cur = remote.cursor(buffered=True)
-# query = "create table iot_project(f2_dist int, f2_timelog varchar(64), f2_video LONGBLOB, f2_timelog_end varchar(64), f2_motor_angle int);"
-# cur.execute(query)
+# query_create_table = "create table iot_project_f2(f2_id int, f2_index int, f2_dist_1 int, f2_dist_2 int, f2_timelog varchar(64), f2_video LONGBLOB, f2_stepmotor_angle int);"
+# cur.execute(query_create_table)
 
 # '------------------변수 선언-------------------'
+f2_id = 0  # 몇번째 인식인지 알려주는 id
 f2_index = 0  # 인덱스
 danger_distance = 5  # 접근 금지 거리[cm]
 filter_sec = 3  # 해당 초 내에 다시 감지되는 경우 같은 경우로 인지
 now = datetime.now()
 
-# '--------f2_index, f2_dist, f2_timelog--------'
+# '--------f2_id, f2_index, f2_dist_1, f2_dist_2, f2_timelog, f2_stepmotor_angle--------'
 def read_arduino_safitysensor():
-    global f2_index, now
+    global f2_id, f2_index, now
     if ardu.readable():
         try:
             # 시리얼 모니터 출력 값
@@ -45,25 +46,24 @@ def read_arduino_safitysensor():
             # print(serial_data)
 
             if serial_data.find("Distance") > -1:  # "\nDistance: "로 시리얼 프린트하므로 split
-                f2_dist = int(serial_data.split(":")[1])  # 초음파 거리값[cm]
-                print(f2_dist)
+                f2_dist_1 = int(serial_data.split(":")[1])  # 초음파 거리값[cm]
+                f2_dist_2 = int(serial_data.split(":")[2])
+                f2_stepmotor_angle = float(serial_data.split(":")[3])
+                # print(f2_dist_1)
 
-                if f2_dist <= danger_distance:  # 테스트를 위해 임의로 5[cm]로 지정
+                if (f2_dist_1 <= danger_distance) or (f2_dist_2 <= danger_distance):  # 테스트를 위해 임의로 5[cm]로 지정
                     print(f2_index)
 
                     now = datetime.now()
                     f2_timelog = str(now.strftime('%Y-%m-%d %H:%M:%S'))
                     print('접근시간: ', now)
 
-                    # f2_video = 
-                    # f2_motor_angle =  # 나중에 추가
-
-                    query_2 = "insert into iot_test (f2_index, f2_dist, f2_timelog) values (%s, %s, %s);"
-                    cur.execute(query_2, ((f2_index, f2_dist, f2_timelog)))
+                    query_2 = "insert into iot_project_f2 (f2_id, f2_index, f2_dist_1, f2_dist_2, f2_timelog, f2_stepmotor_angle) values (%s, %s, %s, %s, %s, %s);"
+                    cur.execute(query_2, ((f2_id, f2_index, f2_dist_1, f2_dist_2, f2_timelog, f2_stepmotor_angle)))
                     remote.commit()
                     
                     # DB에 잘 insert되었는지 터미널에 프린트
-                    query_3 = "select * from iot_test order by f2_timelog desc limit 1;"
+                    query_3 = "select * from iot_project_f2 order by f2_timelog desc limit 1;"
                     cur.execute(query_3)
 
                     result = cur.fetchall()
@@ -75,13 +75,14 @@ def read_arduino_safitysensor():
                 f2_timelog_last = now
                 print('마지막 접근시간: ', f2_timelog_last)
                     
-                if f2_dist > danger_distance:
+                if (f2_dist_1 > danger_distance) and (f2_dist_2 > danger_distance):
                     now_2 = datetime.now()
                     # print(now_2)
                     diff = now_2 - f2_timelog_last
                     print('경과시간: ', diff.seconds)
                     
                     if (diff.seconds > filter_sec):
+                        f2_id += 1
                         f2_index = 0
 
         except Exception as ex:
